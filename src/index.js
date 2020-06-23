a2cc7eab2de6dea9710a3f40e0aa645efe944bb3
Frida Värnlund <frida.varnlund@retursystem.se>
2020-06-23 16:59:33 +0200

    test frida

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
 );
 
 
-// heeej If you want your app to work offline and load faster, you can change
+// Frida If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
